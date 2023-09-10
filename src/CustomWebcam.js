47956bd7204c0578ab23aebf7a98d88aace84dbf
Swarnali Ghosh <swarnalighosh225@gmail.com>
2023-09-10 19:33:56 +0530

    first commit

---
+++ src/CustomWebcam.js
@@ -0,0 +1,33 @@
+import Webcam from "react-webcam";
+import React, { useCallback, useRef, useState } from "react"; // import useRef
+
+const CustomWebcam = () => {
+    const webcamRef = React.useRef(null);
+    const [imgSrc, setImgSrc] = React.useState(null);
+
+    const capture = React.useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        setImgSrc(imageSrc);
+    }, [webcamRef, setImgSrc]);
+
+    console.log(imgSrc);
+
+    return (
+        <>
+            <Webcam
+                audio={false}
+                ref={webcamRef}
+                screenshotFormat="image/jpeg"
+            />
+            <button onClick={capture}>Capture photo</button>
+            {imgSrc && (
+                <img
+                    src={imgSrc}
+                />
+            )}
+
+        </>
+    );
+};
+
+export default CustomWebcam;
